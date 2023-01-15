4a9c88b178f5a23b17f1b1d6c5b994192154a1ba
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-01-15 13:01:58 -0500

    ..

--- resources/views/livewire/slide/show.blade.php
+++ resources/views/livewire/slide/show.blade.php
@@ -26,18 +26,21 @@
 
         @endforelse
     </div>
-    <div class="grid grid-cols-2">
-        <button id="prevBtn" class="hover:bg-slate-200 transition-all py-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-        </button>
-        <button id="nextBtn" class="hover:bg-slate-200 transition-all py-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 float-right mr-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-        </button>
-    </div>
+
+    @if ($slides->count() > 1)
+        <div class="grid grid-cols-2">
+            <button id="prevBtn" class="hover:bg-slate-200 transition-all py-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </button>
+            <button id="nextBtn" class="hover:bg-slate-200 transition-all py-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 float-right mr-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </button>
+        </div>
+    @endif
 
     <script>
         let autoSlide = setInterval(function(){
